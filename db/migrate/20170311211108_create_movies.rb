adae51b5227589c52b42cdf2fc5f75dc2c5292f1
juandavidcowboy88 <ing.juandavid.alvarez@gmail.com>
2017-03-11 16:11:17 -0500

    2

--- db/migrate/20170311211108_create_movies.rb
+++ db/migrate/20170311211108_create_movies.rb
@@ -2,8 +2,8 @@ class CreateMovies < ActiveRecord::Migration[5.0]
   def change
     create_table :movies do |t|
       t.string :name
-      t.integrer :duration
-      t.integrer :year
+      t.integer :duration
+      t.integer :year
       t.string :rating
       t.string :description
       t.string :image_url
